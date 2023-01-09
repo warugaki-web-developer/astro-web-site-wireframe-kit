3932d6719af9e3ad0e95995da90857b1357fa03c
warugaki <warugaki.web.developer@gmail.com>
2023-01-09 16:00:04 +0900

    stylelintの設定

---
+++ .stylelintrc.cjs
@@ -0,0 +1,11 @@
+module.exports = {
+  extends: [
+    'stylelint-config-html',
+    'stylelint-config-standard',
+    'stylelint-config-recess-order',
+    'stylelint-config-prettier',
+  ],
+  rules: {
+    'selector-class-pattern': '^[a-z][a-zA-Z0-9]+$',
+  },
+}
